c12dc040f775ef523cd57d2ab61f3fc7c233510e
Legendary Spaceman <elshenam@oregonstate.edu>
2026-05-30 14:27:39 -0700

    Fix port for Cloud Run deployment

--- server.js
+++ server.js
@@ -52,4 +52,5 @@ app.post('/chat', async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('LandingPad running on http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`LandingPad running on port ${PORT}`));
